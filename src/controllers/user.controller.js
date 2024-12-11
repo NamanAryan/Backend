480883e0e0e2baf7e898d50bc04dcf8da6f28a34
NamanAryan <namanaryan08@gmail.com>
2024-12-11 19:32:34 +0530

    POST REQ

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import asyncHandler from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    console.log("Register endpoint hit");
+    console.log("Request Body:", req.body);
+
+    res.status(200).json({
+        message: "ok",
+    });
+});
+
+export { registerUser };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'dotenv/config'
+import express from "express"
 import connectDB from "./db/index.js";
+const app = express()
 
 connectDB().then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port ${PORT}`);   
+        console.log(`Server is running at port ${process.env.PORT}`);   
     })
 })
