75d5f6d65de6a894f528746c7f18d5224f22c655
NamanAryan <namanaryan08@gmail.com>
2024-12-11 14:24:23 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${PORT}`);   
+    })
+})
